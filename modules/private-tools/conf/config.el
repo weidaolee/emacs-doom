1d266def31bb0d1ac5b4d51ecec83ec293d56971
weidaolee <weidaolee@gmail.com>
2023-09-09 17:52:37 +0800

    feat(private-tools): Support conf-mode for config files.

---
+++ modules/private-tools/conf/config.el
@@ -0,0 +1,5 @@
+;;; private-tools/conf/config.el -*- lexical-binding: t; -*-
+
+(add-to-list 'auto-mode-alist '("\\.cnf\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.conf\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.config\\'" . conf-unix-mode))
